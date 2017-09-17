ae3b00672292bc53bcb1d803d24bffd5e30bc41c
MaximSysoev <time_amf@mail.ru>
2017-09-17 22:49:08 +0300

    task #

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean is(int a, int b) {
+        boolean result = x==x*a+b ? true : false;
+        return result;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PointTest {
+    public void whenPointOnLineThenTrue() {
+        Point a = new Point(1,1);
+        boolean rsl = a.is(0, 1);
+        assertThat(rsl, is(true));
+    }
+}
