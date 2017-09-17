b124a4a04bc31f587dd53234a853060faaf8a5a4
MaximSysoev <time_amf@mail.ru>
2017-09-18 00:44:55 +0300

    maximum 189

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,4 +1,8 @@
 package ru.job4j.max;
 
 public class Max {
+    public int max(int first, int second) {
+        int maxvalue = first  < second ? second : first;
+        return maxvalue;
+    }
 }
