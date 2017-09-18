f89ee89211f46d7b634d5c0af00b91039361beb3
MaximSysoev <time_amf@mail.ru>
2017-09-19 00:18:25 +0300

    task #4412#

--- chapter_001/src/main/java/ru/job4j/condition/Paint.java
+++ chapter_001/src/main/java/ru/job4j/condition/Paint.java
@@ -1,4 +1,17 @@
 package ru.job4j.condition;
 
+
 public class Paint {
-}
+    public String piramid(int h) {
+        StringBuilder builder = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i <= h; i++) {
+            for (int j = 1; j<=count; j++) {
+                builder.append("^");
+            }
+            count+=2;
+            builder.append(System.getProperty("line.separator"));
+        }
+        return  builder.toString();
+    }
+ }
