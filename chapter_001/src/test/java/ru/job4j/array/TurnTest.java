4816d3dd7d81764514df7a5c604b6ded27614694
MaximSysoev <time_amf@mail.ru>
2017-09-19 22:24:07 +0300

    task #4441

---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TurnTest {
+
+    public void RotateFiveElements() {
+        Turn turn = new Turn();
+        int[] resultArray = {1, 2, 3, 4, 5};
+        int[] expectArray = {5, 4, 3, 2, 1};
+        assertThat(resultArray, is(expectArray));
+    }
+
+    public void RotateFourElements() {
+        Turn turn = new Turn();
+        int[] resultArray = {4, 1, 6, 2};
+        int[] expectArray = {2, 6, 1, 4};
+        assertThat(resultArray, is(expectArray));
+    }
+
+}
