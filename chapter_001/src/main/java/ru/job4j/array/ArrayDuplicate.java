77b3ef93b6bd327cf259745f3dc5e12f96f7acd9
MaximSysoev <time_amf@mail.ru>
2017-09-26 00:03:55 +0300

    edit #225

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -2,38 +2,17 @@ package ru.job4j.array;
 import java.util.Arrays;
 
 public class ArrayDuplicate {
-    public String[] remove(String[] list) {
-        int i = 0, k = 0, index = 0;
-
-        String str = "", newStr = "";
-
-        for (i = 0; i < list.length; i++) {
-            k = i + 1;
-            while (k < list.length) {
-                if (list[i] == list[k]) {
-                    list[k] = null;
+    public String[] removeDuplicates(String[] array) {
+        int unique = array.length;
+        for (int out = 0; out < unique; out++) {
+            for (int in = out + 1; in < unique; in++) {
+                if (array[out].equals(array[in])) {
+                    array[in] = array[unique - 1];
+                    unique--;
+                    in--;
                 }
-                k++;
             }
         }
-
-        for (i = 0; i < list.length; i++) {
-            if (list[i] != null) {
-                str = str + list[i] + " ";
-            }
-            list[i]=null;
-        }
-
-        for (char element : str.toCharArray()) {
-            if (element != ' ') {
-                newStr = newStr + element;
-            } else {
-                list[index] = newStr;
-                newStr = "";
-                index++;
-            }
-        }
-
-        return Arrays.copyOf(list, 3);
+        return Arrays.copyOf(array, unique);
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -1,16 +1,18 @@
 package ru.job4j.array;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
 import static org.junit.Assert.assertThat;
+
 public class ArrayDuplicateTest {
 
     @Test
     public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
-        ArrayDuplicate arr = new ArrayDuplicate();
-        String[] list = {"Привет", "Мир", "Привет", "Супер", "Мир"};
-        String [] resultArray = arr.remove(list);
+        String[] input = {"Привет", "Мир", "Привет", "Супер", "Мир"};
         String[] expect = {"Привет", "Мир", "Супер"};
-        assertThat(resultArray, is(expect));
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String [] result = duplicate.removeDuplicates(input);
+        assertThat(result, arrayContainingInAnyOrder(expect));
     }
 
 }
