43df8da53c10f7381cb105efd16f443c208b5e15
MaximSysoev <time_amf@mail.ru>
2017-09-27 22:12:55 +0300

    edit task #228

--- chapter_001/src/main/java/ru/job4j/array/ContainSubString.java
+++ chapter_001/src/main/java/ru/job4j/array/ContainSubString.java
@@ -5,21 +5,18 @@ public class ContainSubString {
     public boolean contains(String origin, String sub) {
         String subTest="";
         for (int i = 0; i < origin.length(); i++) {
-            for (int j = 0; j < sub.length(); j++) {
-                if (origin.charAt(i)==sub.charAt(j)) {
-                    subTest = subTest + origin.charAt(i);
-                    i++;
-                } else {
-                    break;
+            if (origin.charAt(i) == sub.charAt(0)) {
+                subTest = "";
+                subTest =subTest + sub.charAt(0);
+                for (int j = 1; j < sub.length(); j++) {
+                    if (origin.charAt(i + j) == sub.charAt(j)) {
+                        subTest = subTest + origin.charAt(i + j);
+                        i = subTest.equals(sub) ? origin.length() : i;
+                    }
                 }
             }
         }
-
-        if (subTest.equals(sub)) {
-            return true;
-        } else {
-            return false;
-        }
+        return (subTest.equals(sub)) ? true : false;
     }
 
 }
