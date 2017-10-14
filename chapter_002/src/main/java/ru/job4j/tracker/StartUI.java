b7ca9e1548db0673ee9fe46e7ef1664e6a6ba790
MaximSysoev <time_amf@mail.ru>
2017-10-14 11:12:45 +0300

    ConsoleInput в классе StartUI

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,4 +1,13 @@
 package ru.job4j.tracker;
+import java.util.Scanner;
 
 public class StartUI {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ConsoleInput input = new ConsoleInput();
+        input.menu();
+        String number = scanner.nextLine();
+        input.selectMenu(number);
+    }
+
 }
---
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,8 @@
+package ru.job4j.tracker;
+
+public class StartUITest {
+  /*  public static void main(String[] args) {
+        Input input = new StubInput(new String[] {"Hello"});
+        new StartUI(input).init();
+    }*/
+}
