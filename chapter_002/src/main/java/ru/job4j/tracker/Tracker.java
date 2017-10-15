b9d95a1ea977578c7a49604ad515da564a6a22be
MaximSysoev <time_amf@mail.ru>
2017-10-15 14:26:53 +0300

    2. Реализовать класс Tracker [ #396]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -7,6 +7,15 @@ public class Tracker {
     private int position = 0;
     private static final Random RN = new Random();
 
+    public void assingValue(Item value) {
+        for (int index = 0; index < this.position; index++) {
+            if (this.items[index].getId().equals(value.getId())) {
+                this.items[index] = value;
+                break;
+            }
+        }
+    }
+
     public Item add(Item item) {
         item.setId(this.generateId());
         this.items[position++] = item;
@@ -14,36 +23,26 @@ public class Tracker {
     }
 
     public void update(Item item) {
-        for (int index = 0; index < this.position; index++) {
-            if (this.items[index].getId().equals(item.getId())) {
-                this.items[index] = item;
-            }
-        }
+        this.assingValue(item);
     }
 
     public void delete(Item item) {
-        for (int index = 0; index < this.position; index++) {
-            if (this.items[index].getId().equals(item.getId())) {
-                this.items[index] = null;
-            }
-        }
+       this.assingValue(null);
     }
-
 
     public Item[] findAll() {
         Item[] result = new Item[this.position];
         int k = 0;
         for (int index = 0; index < this.position; index++) {
            if (this.items[index] != null) {
-               result[k] = this.items[index];
-               k++;
+               result[k++] = this.items[index];
             }
         }
         return result;
     }
 
     public Item[] findByName(String key) {
-        int k = 0;
+        int k = 0, j = 0;
         for (int index = 0; index < this.position; index++) {
             if (this.items[index].getName().equals(key)) {
                 k++;
@@ -51,10 +50,12 @@ public class Tracker {
         }
 
         Item[] result = new Item[k];
-        for (int index = 0; index < k; index++) {
-            result[k] = this.items[index];
+        for (int index = 0; index < this.position; index++) {
+            if (this.items[index].getName().equals(key)) {
+                result[j] = this.items[index];
+                j++;
+            }
         }
-
         return result;
     }
 
