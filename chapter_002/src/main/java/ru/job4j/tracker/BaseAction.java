d16918e23f5e376ea8712875cc62d0544b14394f
MaximSysoev <time_amf@mail.ru>
2017-10-24 19:29:31 +0300

    task #790

--- chapter_002/src/main/java/ru/job4j/tracker/BaseAction.java
+++ chapter_002/src/main/java/ru/job4j/tracker/BaseAction.java
@@ -1,4 +1,50 @@
 package ru.job4j.tracker;
 
-public class BaseAction {
+/**
+ * Абстрактный класс baseAction.
+ */
+abstract class BaseAction implements UserAction {
+    /**
+     * Переменная name.
+     */
+    private String name;
+
+    /**
+     * Переменная key.
+     */
+    private int key;
+
+    /**
+     * Конструктор класса BaseAction.
+     * @param key параметр.
+     * @param name параметр.
+     */
+    BaseAction(int key, String name) {
+        this.key = key;
+        this.name = name;
+    }
+
+    /**
+     * Установка цифры для интерфейса.
+     * @return цифру(ключ).
+     */
+    public int key() {
+        return this.key;
+    }
+
+    /**
+     * Метод execute – иниициализация параметров для методов.
+     * @param input параметр.
+     * @param tracker параметр.
+     */
+    public abstract void execute(Input input, Tracker tracker);
+
+    /**
+     * Вывод строчки информации.
+     * @return String.format.
+     */
+    public String info() {
+        return String.format("%s %s", this.key(), this.name);
+    }
+
 }
