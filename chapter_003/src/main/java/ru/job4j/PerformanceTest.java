a2941aced60dc9c6e1039d19aed993b76e7acf69
MaximSysoev <time_amf@mail.ru>
2017-12-02 18:37:21 +0300

    task #10022

--- chapter_003/src/main/java/ru/job4j/PerformanceTest.java
+++ chapter_003/src/main/java/ru/job4j/PerformanceTest.java
@@ -1,4 +1,33 @@
 package ru.job4j;
 
+import java.util.*;
+
+/**
+ * PerformanceTest - в этом классе тестируются коллекции.
+ */
 public class PerformanceTest {
+    /**
+     * Точка входа в программу.
+     * @param args – аргументы метода.
+     */
+    public static void main(String[] args) {
+        ArrList testArrList = new ArrList();
+        LinkList testLinkList = new LinkList();
+        TreSet testTreSet = new TreSet();
+
+        ArrayList<String> al = new ArrayList<String>();
+        LinkedList<String> ll = new LinkedList<String>();
+        TreeSet<String> ts = new TreeSet<String>();
+
+        long timeAddAl = testArrList.add(al, 100);
+        long timeDelAl = testArrList.delete(al, 10);
+        long timeAddLl = testLinkList.add(ll, 100);
+        long timeDelLl = testLinkList.delete(ll, 10);
+        long timeAddTs = testTreSet.add(ts, 100);
+        long timeDelTs = testTreSet.delete(ts, 10);
+
+        System.out.println("Add ArrayList:  " + timeAddAl + "   |   Del ArrayList:  " + timeDelAl);
+        System.out.println("Add LinkedList: " + timeAddLl + "   |   Del LinkedList: " + timeDelLl);
+        System.out.println("Add TreeSet:    " + timeAddTs + "   |   Del TreeSet:    " + timeDelTs);
+    }
 }
--- chapter_003/src/main/java/ru/job4j/TreSet.java
+++ chapter_003/src/main/java/ru/job4j/TreSet.java
@@ -1,4 +1,49 @@
 package ru.job4j;
 
+import java.util.*;
+
+/**
+ * Класс ArrList - работа с колеекцией ArrayList.
+ */
 public class TreSet {
+    /**
+     * Генератор случайных чисел.
+     */
+    private Random rand = new Random();
+
+    /**
+     * Метод add заполняет коллекцию случайными значениями.
+     * @param collection - коллекция.
+     * @param amount - количество.
+     * @return timeConsumedMills - Время выполнения метода add.
+     */
+    public long add(Collection<String> collection, int amount) {
+        long start = System.nanoTime();
+        for (int i = 0; i < amount; i++) {
+            collection.add(String.valueOf(rand.nextInt(100)));
+        }
+        long finish = System.nanoTime();
+        long timeConsumedMills = finish - start;
+        return timeConsumedMills;
+    }
+
+    /**
+     * Метод delete удаляет из коллекции элементы.
+     * @param collection - коллекция.
+     * @param amount - количество.
+     * @return timeConsumedMills - Время выполнения метода delete.
+     */
+    public long delete(Collection<String> collection, int amount) {
+        TreeSet<String> ts = new TreeSet<String>(collection);
+        int count = 0;
+        long start = System.nanoTime();
+        while (count < amount) {
+            ts.pollFirst();
+            count++;
+        }
+        long finish = System.nanoTime();
+        long timeConsumedMills = finish - start;
+        return timeConsumedMills;
+    }
+
 }
