a8d4f34b1f2c5f8f3b142d6ad42d6dfdb784182f
MaximSysoev <time_amf@mail.ru>
2018-01-08 18:12:30 +0300

    Доп. задание

--- chapter003/src/main/java/ru/job4j/sortdepartment/DepartmentList.java
+++ chapter003/src/main/java/ru/job4j/sortdepartment/DepartmentList.java
@@ -1,97 +1,92 @@
 package ru.job4j.sortdepartment;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DepartmentList{
 
     private String department;
 
     public int compare(int[] left, int[] right) {
-        int result = 1;
-        int count = 0;
-        int sizeLeft = left.length;
-        int sizeRight = right.length;
-        if (sizeLeft == sizeRight - 1) {
-            for (int i = 0; i < sizeLeft; i++) {
-                if (left[i] == right[i]) {
-                    count++;
-                }
-                if (count == sizeLeft) {
-                    result = -1;
-                }
-            }
-        } else if (sizeLeft == sizeRight) {
-            for (int i = 0; i < sizeLeft; i++) {
-                if (left[i] == right[i]) {
-                    count++;
-                }
-                if (count == sizeLeft) {
-                    result = 0;
-                } else {
-                    result = 1;
-                }
+        int result = 0;
+        int diff = left.length - right.length;
+        for (int index = 0; index < Math.abs(diff); index++) {
+            result = Integer.compare(left[index], right[index]);
+            if (result != 0) {
+                break;
             }
         }
+        if (result == 0) {
+            result = diff;
+        }
+        System.out.println(result);
         return result;
     }
 
-    public String getDepartment() {
-        return this.department;
+
+    public List<List<String>> convert(Set<String> list) {
+        List<List<String>> lst = new ArrayList<>();
+        int index = 1;
+        for (String s : list) {
+            lst.add(Arrays.asList(s.split("\\\\")));
+
+        }
+
+        System.out.print(lst + " / " + lst.size());
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < lst.size(); i+=2) {
+            if (index < lst.size()) {
+                int[] left = new int[lst.get(i).size()];
+                int[] right = new int[lst.get(index).size()];
+                int k = 0;
+                for (String s : lst.get(i)) {
+                    left[k] = Integer.parseInt(s.substring(s.length()-1));
+                }
+
+                for (String s : lst.get(index)) {
+                    right[k] = Integer.parseInt(s.substring(s.length()-1));
+                }
+                index+=2;
+                this.compare(left, right);
+            } else {
+                index-=2;
+                int[] left = new int[lst.get(i).size()];
+                int[] right = new int[lst.get(index).size()];
+                int k = 0;
+                for (String s : lst.get(i)) {
+                    left[k] = Integer.parseInt(s.substring(s.length()-1));
+                }
+
+                for (String s : lst.get(index)) {
+                    right[k] = Integer.parseInt(s.substring(s.length()-1));
+                }
+                this.compare(left, right);
+            }
+
+        }
+        return lst;
     }
 
     public static void main(String[] args) {
-        TreeSet<String> treeSet = new TreeSet<String>();
-        int k = 0;
-        int count = 1;
-        String dep;
-        treeSet.addAll(
-            Arrays.asList(
-                 "K1\\SK1",
-                 "K2\\SK1\\SSK1",
-                 "K1\\SK2",
-                 "K1\\SK1\\SSK1",
-                 "K2",
-                 "K1\\SK1\\SSK2",
-                 "K2\\SK1\\SSK2"
-            )
-        );
-
         DepartmentList departmentList = new DepartmentList();
-        for (int i=0; i<treeSet.toArray().length; i+=2) {
-            if (i+1 < treeSet.toArray().length) {
-                int ch = i + 1;
-                String[] listLeft = treeSet.toArray()[i].toString().split("\\\\"); // Разбиваю массив.
-                String[] listRight = treeSet.toArray()[ch].toString().split("\\\\"); // Разбиваю массив.
-                int[] left = new int[listLeft.length];
-                int[] right = new int[listRight.length];
-                for (int j = 0; j < listLeft.length; j++) {
-                    k = Integer.parseInt(listLeft[j].substring(listLeft[j].length()-1));
-                    left[j] = k;
-                }
-                for (int j = 0; j < listRight.length; j++) {
-                    k = Integer.parseInt(listRight[j].substring(listRight[j].length()-1));
-                    right[j] = k;
-                }
-                int result = departmentList.compare(left, right);
-                dep = "";
-                if (result == 1 && left.length < right.length) {
-                    k = right.length - left.length;
-                    for (int j = 0; j < k; j++) {
-                        if (j!=1) {
-                            dep = dep + "K" + right[j] + "\\";
-                        } else {
-                            dep = dep + "SK" + right[j];
-                        }
-                    }
-                } else if (result == -1 && left.length > 1) {
-                    treeSet.add("K" + left[0]);
-                }
-                treeSet.add(dep);
-            }
-        }
+        TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.addAll(
+                Arrays.asList(
+                        "K1\\SK1",
+                        "K2\\SK1\\SSK1",
+                        "K1\\SK2",
+                        "K1\\SK1\\SSK1",
+                        "K2",
+                        "K1\\SK1\\SSK2",
+                        "K2\\SK1\\SSK2"
+                )
+        );
+        List<List<String>> lst = departmentList.convert(treeSet);
 
-    for (String list : treeSet) {
-            System.out.println(list);
-        }
+        int result = departmentList.compare(new int[] {1,1,2}, new int[] {1, 2});
 
     }
+
 }
