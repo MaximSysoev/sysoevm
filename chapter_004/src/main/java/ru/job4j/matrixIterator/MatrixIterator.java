01a25d653b3dbacc29b42ffcb7cfb15d9162e83c
MaximSysoev <time_amf@mail.ru>
2018-01-26 19:44:37 +0300

    edit task #9539#38407

--- chapter_004/src/main/java/ru/job4j/matrixIterator/MatrixIterator.java
+++ chapter_004/src/main/java/ru/job4j/matrixIterator/MatrixIterator.java
@@ -6,29 +6,55 @@ public class MatrixIterator implements Iterator {
     private int[][] values;
     private int length = 0;
 
+
     public MatrixIterator(int[][] values) {
         this.values = values;
     }
 
+    public int getDemension() {
+        int dimension = 0;
+        for (int i = 0; i < values.length; i++) {
+            dimension = dimension + values[i].length;
+        }
+        return dimension;
+    }
+
     @Override
     public boolean hasNext() {
-        return values[0].length+values[1].length > length;
+        return getDemension() > length;
     }
 
     @Override
     public Object next() {
         int count = 0;
         int value = 0;
-        for (int i = 0; i < values.length; i++) {
-            for (int j = 0; j < values[i].length; j++) {
-                if (length==count) {
-                    value = values[i][j];
+        if (length < getDemension()) {
+            for (int i = 0; i < values.length; i++) {
+                for (int j = 0; j < values[i].length; j++) {
+                    if (length == count) {
+                        value = values[i][j];
+                    }
+                    count++;
                 }
-                count++;
             }
+            length++;
+        } else {
+            throw new NoSuchElementException();
         }
-        length++;
+
         return value;
+    }
+
+    public static void main(String[] args) {
+        Iterator<Integer> it = new MatrixIterator(new int[][] {{1}, {3, 4}, {7}, {12, 16, 24, 2}});
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
     }
 
 }
