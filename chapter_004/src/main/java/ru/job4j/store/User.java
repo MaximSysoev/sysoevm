613728dd8f6eb40ca100ac8ddb1ca7c26fca3ada
MaximSysoev <time_amf@mail.ru>
2018-02-07 20:30:01 +0300

    task #157

--- chapter_004/src/main/java/ru/job4j/store/User.java
+++ chapter_004/src/main/java/ru/job4j/store/User.java
@@ -6,17 +6,4 @@ public class User extends Base {
     public User(String id) {
         super(id);
     }
-
-    public static void main(String[] args) {
-        UserStore userStore = new UserStore();
-        User user1 = new User("1");
-        User user2 = new User("2");
-        User user3 = new User("3");
-        userStore.add(user1);
-        userStore.add(user2);
-        userStore.add(user3);
-        userStore.delete(user1.getId());
-        System.out.println(userStore.simpleArray.arrayList.get(0).getId());
-        System.out.println(userStore.findById(user2.getId()));
-    }
 }
