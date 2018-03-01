c37ab7545d5435437260d5354475351e6a964447
MaximSysoev <time_amf@mail.ru>
2018-03-01 18:38:35 +0300

    task #159

--- chapter_004/src/main/java/ru/job4j/listNode/Container.java
+++ chapter_004/src/main/java/ru/job4j/listNode/Container.java
@@ -1,28 +1,47 @@
 package ru.job4j.listNode;
-
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class Container<E> implements Iterator<E> {
+public class Container<E> implements Iterable<E> {
+
     public Node<E> head;
     public Node<E> tail;
+    public Node<E> temp = head;
 
     public void add(E value) {
+        Node<E> newNode = new Node<>(value);
         if (head == null) {
-            head = new Node<>(value);
-            tail = head;
+            temp = head = tail = newNode;  // и head и tail ссылаются на новый узел. Потому что в списке пока только один узел.
         } else {
-            head.next = new Node<>(value);
-            tail = head.next;
+            tail.next = newNode;    // последний узел начинает ссылаться на новый узел, который добавляется в конец
+            tail = tail.next; // перекидываем ссылку tail на новый, только что добавленный в предыдущей строке узел
         }
     }
 
     @Override
-    public boolean hasNext() {
-        return head.next!=null;
+    public Iterator<E> iterator() {
+        return new simpleIterator();
     }
 
-    @Override
-    public E next() {
-        return head.next.value;
+    class simpleIterator implements Iterator<E> {
+
+        E value = null;
+        @Override
+        public boolean hasNext() {
+            if (temp.value!=null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public E next() {
+            value = temp.value;
+            temp = temp.next;
+            return value;
+        }
     }
 }
+
