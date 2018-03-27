a20b7f60246ac23c1ae54641acfe1a861d401528
MaximSysoev <time_amf@mail.ru>
2018-03-27 08:14:09 +0300

    task #999

---
+++ chapter_004/src/main/java/ru/job4j/user/Calendar.java
@@ -0,0 +1,4 @@
+package ru.job4j.user;
+
+public class Calendar {
+}
---
+++ chapter_004/src/main/java/ru/job4j/user/User.java
@@ -0,0 +1,11 @@
+package ru.job4j.user;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+
+    }
+}
