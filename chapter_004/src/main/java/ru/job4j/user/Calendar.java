bf5ee64743762447b6c670520ed56cdbc40d57df
MaximSysoev <time_amf@mail.ru>
2018-03-28 19:07:57 +0300

    task #999

--- chapter_004/src/main/java/ru/job4j/user/Calendar.java
+++
@@ -1,4 +0,0 @@
-package ru.job4j.user;
-
-public class Calendar {
-}
--- chapter_004/src/main/java/ru/job4j/user/User.java
+++ chapter_004/src/main/java/ru/job4j/user/User.java
@@ -1,11 +1,39 @@
 package ru.job4j.user;
+import java.util.*;
 
 public class User {
-    private String name;
-    private int children;
-    private Calendar birthday;
 
-    public User(String name, int children, Calendar birthday) {
+    public String name;
+    public int children;
+    Calendar birthday;
 
+    public User(String name, int children) {
+        this.name = name;
+        this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        User user = (User) o;
+
+        if (name != null ? !name.equals(user.name) : user.name != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
     }
 }
+
