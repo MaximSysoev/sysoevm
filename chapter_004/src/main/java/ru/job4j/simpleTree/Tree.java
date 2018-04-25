2c9796a7675711974ce21fceaebda9667c4e3ff5
Maxim Sysoev <time_amf@mail.ru>
2018-04-25 23:42:43 +0300

    task #1711

--- chapter_004/src/main/java/ru/job4j/simpleTree/Tree.java
+++ chapter_004/src/main/java/ru/job4j/simpleTree/Tree.java
@@ -11,8 +11,8 @@ public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
 
     @Override
     public boolean add(E parent, E child) {
-        if (findBy(parent).isPresent()) { // Если есть родительский элемент.
-            Node<E> element = (Node)findBy(parent).get();
+        if (findBy(parent).isPresent()) {
+            Node<E> element = findBy(parent).get();
             element.add(new Node<>(child));
             return true;
         } else {
@@ -21,12 +21,12 @@ public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
     }
 
     @Override
-    public Optional findBy(E value) {
-        Optional<Node<E>> rsl = Optional.empty(); // Создаётся объект типа optional.
-        Queue<Node<E>> data = new LinkedList<>(); // Создаётся объект типа Queue
-        data.offer(this.root); // добавляется в очередь объект root
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
         while (!data.isEmpty()) {
-            Node<E> el = data.poll(); // Получаем объект из очереди.
+            Node<E> el = data.poll();
             if (el.eqValue(value)) {
                 rsl = Optional.of(el);
                 break;
@@ -38,8 +38,23 @@ public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
         return rsl;
     }
 
+    public class treeIterator<E> implements Iterator {
+
+        List next = root.leaves();
+
+        @Override
+        public boolean hasNext() {
+            return next != null;
+        }
+
+        @Override
+        public Object next() {
+            return next.iterator().next();
+        }
+    }
+
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new treeIterator();
     }
 }
