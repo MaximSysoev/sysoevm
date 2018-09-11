16ff1d309ea0b2853153b1b89a471797a3369096
Maxim Sysoev <time_amf@mail.ru>
2018-09-11 21:21:02 +0300

    task #1734

--- chapter_002/src/main/java/ru/job4j/tracker/SQLStorage.java
+++ chapter_002/src/main/java/ru/job4j/tracker/SQLStorage.java
@@ -17,8 +17,7 @@ public class SQLStorage implements AutoCloseable {
             conn = DriverManager.getConnection(url, username, password);
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery("SELECT * FROM users");
-            while (rs.next())
-            {
+            while (rs.next()) {
                 System.out.println(String.format("%s", rs.getInt("id")));
             }
 
