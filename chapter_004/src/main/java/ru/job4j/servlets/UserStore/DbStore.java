1c1b22c7d934e724a7614ee9d52d5806e74833d5
Maxim Sysoev <time_amf@mail.ru>
2018-12-22 00:57:58 +0300

    task #51922 #98787

--- chapter_004/src/main/java/ru/job4j/servlets/UserStore/DbStore.java
+++ chapter_004/src/main/java/ru/job4j/servlets/UserStore/DbStore.java
@@ -10,50 +10,65 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class DbStore implements Store {
     private static DbStore instance = new DbStore();
     private static Connect conn = new Connect();
+    private static Connection connection = null;
+    private static Statement st = null;
 
     public static DbStore getInstance() {
         return instance;
     }
 
+    private void closeQuietly() {
+        try {
+            this.st.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            this.connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void add(User user) {
         try {
-            Connection connection = conn.getSOURCE().getConnection();
-            Statement st = connection.prepareStatement("INSERT INTO users(name, login, email) values(?,?,?)");
+            connection = conn.getSOURCE().getConnection();
+            st = connection.prepareStatement("INSERT INTO users(name, login, email) values(?,?,?)");
             ((PreparedStatement) st).setString(1, user.getName());
             ((PreparedStatement) st).setString(2, user.getLogin());
             ((PreparedStatement) st).setString(3, user.getEmail());
             ((PreparedStatement) st).executeQuery();
-            st.close();
-            connection.close();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            this.closeQuietly();
         }
     }
 
     @Override
     public void update(int id, User user) {
         try {
-            Connection connection = conn.getSOURCE().getConnection();
-            Statement st = connection.prepareStatement("UPDATE users SET name='"+user.getName()+"', login='"+user.getLogin()+"', email='"+user.getEmail()+"' WHERE id='"+id+"'");
+            connection = conn.getSOURCE().getConnection();
+            st = connection.prepareStatement("UPDATE users SET name='"+user.getName()+"', login='"+user.getLogin()+"', email='"+user.getEmail()+"' WHERE id='"+id+"'");
             ((PreparedStatement) st).executeQuery();
-            st.close();
-            connection.close();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            this.closeQuietly();
         }
     }
 
     @Override
     public void delete(int key) {
         try {
-            Connection connection = conn.getSOURCE().getConnection();
-            Statement st = connection.prepareStatement("delete from users where id = '"+key+"'");
+            connection = conn.getSOURCE().getConnection();
+            st = connection.prepareStatement("delete from users where id = '"+key+"'");
             ((PreparedStatement) st).executeQuery();
-            st.close();
-            connection.close();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            this.closeQuietly();
         }
     }
 
@@ -61,17 +76,17 @@ public class DbStore implements Store {
     public List<User> findAll() {
         List<User> userStore = new CopyOnWriteArrayList<User>();
         try {
-            Connection connection = conn.getSOURCE().getConnection();
-            Statement st = connection.createStatement();
+            connection = conn.getSOURCE().getConnection();
+            st = connection.createStatement();
             ResultSet rs = st.executeQuery("select * from users");
             while (rs.next()) {
                 User user = new User(rs.getInt("id"), rs.getString("name"), rs.getString("login"), rs.getString("email"), new Date());
                 userStore.add(user);
             }
-            st.close();
-            connection.close();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            this.closeQuietly();
         }
         return userStore;
     }
@@ -80,17 +95,17 @@ public class DbStore implements Store {
     public User findById(int key) {
         User user = new User();
         try {
-            Connection connection = conn.getSOURCE().getConnection();
-            Statement st = connection.createStatement();
+            connection = conn.getSOURCE().getConnection();
+            st = connection.createStatement();
             ResultSet rs = st.executeQuery("select * from users where id = " + key);
             rs.next();
             user.setName(rs.getString("name"));
             user.setLogin(rs.getString("login"));
             user.setEmail(rs.getString("email"));
-            st.close();
-            connection.close();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            this.closeQuietly();
         }
         return user;
     }
