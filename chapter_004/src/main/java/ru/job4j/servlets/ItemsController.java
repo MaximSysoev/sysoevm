4993fc708b2f2dff68393fc45af9f2c00fc3dfab
Maxim Sysoev <time_amf@mail.ru>
2019-01-28 00:15:27 +0300

    task #58534

---
+++ chapter_004/src/main/java/ru/job4j/servlets/ItemsController.java
@@ -0,0 +1,24 @@
+package ru.job4j.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ItemsController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        req.getRequestDispatcher("/WEB-INF/jsp/Items.html").forward(req, resp);
+       // PrintWriter writer = new PrintWriter(resp.getOutputStream());
+       // writer.append("<h3>Hello world</h3>");
+       // writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
