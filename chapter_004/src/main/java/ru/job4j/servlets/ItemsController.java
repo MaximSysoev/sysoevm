b3f494c3dcaffee5fd5a7ecf530e5a36849c8646
Maxim Sysoev <time_amf@mail.ru>
2019-02-08 02:05:56 +0300

    #58534

--- chapter_004/src/main/java/ru/job4j/servlets/ItemsController.java
+++ chapter_004/src/main/java/ru/job4j/servlets/ItemsController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.*;
-import java.security.KeyStore;
 import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -16,29 +15,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class ItemsController extends HttpServlet {
 
     private final static String baseFile = "user.json";
-    ConcurrentHashMap<String, User> map = new ConcurrentHashMap<>();
-    {
-        map.put("1", new User(0, "Andrei", "login", "email@email", new Date(), "password", 2));
-        map.put("2", new User(0, "Maxim", "login2", "email2@email2", new Date(), "password2", 2));
-    }
+    ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //map.put("1", new User(0, "Andrei", "login", "email@email", new Date(), "password", 2));
+        //map.put("2", new User(0, "Maxim", "login2", "email2@email2", new Date(), "password2", 2));
         resp.setContentType("text/json");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
         int count = 0;
+        ObjectMapper mapper = new ObjectMapper();
         StringBuilder builder = new StringBuilder();
-            for (Map.Entry<String,  User> entry : map.entrySet()) {
+            for (Map.Entry<String,  String> entry : map.entrySet()) {
                 if (count!=map.size()-1) {
-                   builder.append("{\'password\' : " + '\'' + entry.getKey() + '\'' + "," + "\'login\':" + '\'' + entry.getValue().getLogin() + '\'' + "}," );
+                   builder.append("{password:"  + entry.getKey()  + "," + "email:"  + entry.getValue() + "}," );
                 } else {
-                    builder.append("{\'password\' : " + '\'' + entry.getKey() + '\'' + "," + "\'login\':" + '\'' + entry.getValue().getLogin() + '\'' + "}" );
+                    builder.append("{\'password\':" + '\'' + entry.getKey() + '\'' + "," + "\'email\':" + '\'' + entry.getValue() + '\'' + "}" );
                 }
                 count++;
         }
-        ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(builder);
-        writer.append("[" + json + "]");
+        mapper.writeValue(writer, map.values().toArray());
+        //String json = mapper.writeValueAsString(builder);
+      //  writer.append("[{\"password\":\"password\", \"login\":\"login\"}, {\"password\":\"password\", \"login\":\"login\"}]");
         writer.flush();
     }
 
@@ -47,5 +46,8 @@ public class ItemsController extends HttpServlet {
         ObjectMapper mapper = new ObjectMapper();
         BufferedReader reader = req.getReader();
         final User user = mapper.convertValue(reader, User.class);
+        String email = req.getParameter("email");
+        String pwd = req.getParameter("pwd");
+        map.put(email, pwd);
     }
 }
