032f9ead1f2d02e1a0770517a178e3a5725a6616
Maxim Sysoev <time_amf@mail.ru>
2019-02-08 20:33:07 +0300

    #58534

--- chapter_004/src/main/java/ru/job4j/servlets/ItemsController.java
+++ chapter_004/src/main/java/ru/job4j/servlets/ItemsController.java
@@ -10,44 +10,40 @@ import java.io.*;
 import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ItemsController extends HttpServlet {
-
-    private final static String baseFile = "user.json";
-    ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
-
+    ConcurrentHashMap<Integer, User> map = new ConcurrentHashMap<>();
+    private AtomicInteger integer = new AtomicInteger(0);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //map.put("1", new User(0, "Andrei", "login", "email@email", new Date(), "password", 2));
-        //map.put("2", new User(0, "Maxim", "login2", "email2@email2", new Date(), "password2", 2));
         resp.setContentType("text/json");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        int count = 0;
         ObjectMapper mapper = new ObjectMapper();
-        StringBuilder builder = new StringBuilder();
-            for (Map.Entry<String,  String> entry : map.entrySet()) {
-                if (count!=map.size()-1) {
-                   builder.append("{password:"  + entry.getKey()  + "," + "email:"  + entry.getValue() + "}," );
-                } else {
-                    builder.append("{\'password\':" + '\'' + entry.getKey() + '\'' + "," + "\'email\':" + '\'' + entry.getValue() + '\'' + "}" );
-                }
-                count++;
-        }
         mapper.writeValue(writer, map.values().toArray());
-        //String json = mapper.writeValueAsString(builder);
-      //  writer.append("[{\"password\":\"password\", \"login\":\"login\"}, {\"password\":\"password\", \"login\":\"login\"}]");
         writer.flush();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        BufferedReader reader = req.getReader();
-        final User user = mapper.convertValue(reader, User.class);
-        String email = req.getParameter("email");
-        String pwd = req.getParameter("pwd");
-        map.put(email, pwd);
+        User user = new User(0, "", "", req.getParameter("email"), new Date(), req.getParameter("password"), 2);
+        map.put(integer.getAndIncrement(), user);
+        /*BufferedReader reader = req.getReader();
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user = objectMapper.readValue(sb.toString(), User.class);
+        map.put(integer.getAndIncrement(), user);
+        String toJson = objectMapper.writeValueAsString(map);
+        resp.setContentType("text/json");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        System.out.println(toJson);
+        writer.append(toJson);
+        writer.flush();*/
     }
 }
