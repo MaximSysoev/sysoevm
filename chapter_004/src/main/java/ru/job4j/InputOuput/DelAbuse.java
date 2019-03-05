0ac1509e97a32172b3cca512016385d65a8b2062
Maxim Sysoev <time_amf@mail.ru>
2019-03-06 02:25:27 +0300

    #task

--- chapter_004/src/main/java/ru/job4j/InputOuput/DelAbuse.java
+++ chapter_004/src/main/java/ru/job4j/InputOuput/DelAbuse.java
@@ -3,11 +3,12 @@ package ru.job4j.inputouput;
 import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DelAbuse  {
     public void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
-        try(BufferedInputStream bis = new BufferedInputStream(in)) {
+       /* try(BufferedInputStream bis = new BufferedInputStream(in)) {
             Scanner sc = new Scanner(bis);
             int count = 0;
             while(sc.hasNext()) {
@@ -24,18 +25,35 @@ public class DelAbuse  {
                 } else {
                     count = 0;
                 }
-
                 System.out.println(out.toString());
             }
 
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
+
+       try (
+           BufferedReader bf = new BufferedReader(new InputStreamReader(in));
+           BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+       ) {
+           String line;
+           while ((line=bf.readLine())!=null) {
+               for(String s : abuse)  {
+                    if (line.contains(s)) {
+                        line = line.replaceAll(s, "");
+                    }
+               }
+               bw.write(line + System.lineSeparator());
+               bw.flush();
+           }
+       } catch (Exception e) {
+            e.printStackTrace();
+       }
     }
 
     public static void main(String[] args) throws IOException {
         DelAbuse delAbuse = new DelAbuse();
         String[] abuse = new String[]{"слово1", "слово2"};
-        delAbuse.dropAbuses(System.in, new ByteOutputStream(), abuse);
+        delAbuse.dropAbuses(System.in, System.out, abuse);
     }
 }
---
+++ chapter_004/src/test/java/ru/job4j/InputOutput/TestDelAbuse.java
@@ -0,0 +1,4 @@
+package ru.job4j.inputoutput;
+
+public class TestDelAbuse {
+}
