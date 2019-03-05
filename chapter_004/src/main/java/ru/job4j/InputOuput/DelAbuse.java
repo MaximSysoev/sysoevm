8f0d8e54a6c5501cdcaad36f83b497ae4e18540c
Maxim Sysoev <time_amf@mail.ru>
2019-03-05 22:28:04 +0300

    #task

--- chapter_004/src/main/java/ru/job4j/InputOuput/DelAbuse.java
+++ chapter_004/src/main/java/ru/job4j/InputOuput/DelAbuse.java
@@ -5,15 +5,31 @@ import java.util.Scanner;
 
 public class DelAbuse  {
     public void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
-        try(
-                BufferedInputStream bis = new BufferedInputStream(in);
-                BufferedOutputStream bos = new BufferedOutputStream(out);
-        ) {
-            Scanner sc = new Scanner(bis);
+        BufferedInputStream bis = new BufferedInputStream(in);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        Scanner sc = new Scanner(bis);
+
+        int count = 0;
+        try {
             while(sc.hasNext()) {
-                byte[] buffer = sc.next().getBytes();
-                bos.write(buffer);
+                String s = sc.next();
+                byte[] buffer = s.getBytes();
+                for (int i = 0; i < abuse.length; i++) {
+                    if (s.equals(abuse[i])) {
+                        count = 1;
+                        break;
+                    }
+                }
+                if (count == 0) {
+                    bos.write(buffer);
+                } else {
+                    count = 0;
+                }
+
+                System.out.println(bos.toString());
             }
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -21,7 +37,7 @@ public class DelAbuse  {
 
     public static void main(String[] args) throws IOException {
         DelAbuse delAbuse = new DelAbuse();
-        String[] abuse = new String[]{"слово1", "слово2"};
+        String[] abuse = new String[]{"плохое", "слово"};
         delAbuse.dropAbuses(System.in, System.out, abuse);
     }
 }
