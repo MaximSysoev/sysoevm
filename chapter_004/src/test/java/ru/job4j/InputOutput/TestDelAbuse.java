9ae464782f5c68740c687cfb97d93ac27495cffd
Maxim Sysoev <time_amf@mail.ru>
2019-03-07 11:45:16 +0300

    #task

--- chapter_004/src/test/java/ru/job4j/InputOutput/TestDelAbuse.java
+++ chapter_004/src/test/java/ru/job4j/InputOutput/TestDelAbuse.java
@@ -1,20 +1,37 @@
 package ru.job4j.inputoutput;
+import org.junit.Before;
 import org.junit.Test;
 import ru.job4j.inputouput.DelAbuse;
 
 import java.io.*;
+import java.security.Provider;
+
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 public class TestDelAbuse {
 
+
+       DelAbuse delAbuse = new DelAbuse();
+        InputStream is = getClass().getClassLoader().getResourceAsStream("input.txt");
+        String[] ab = new String[] {"1111", "22", "33"};
+    InputStream is2 = getClass().getClassLoader().getResourceAsStream("input.txt");
+
+
     @Test
     public void whenContainsAbuse() throws IOException {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        System.setIn(new ByteArrayInputStream("слово1 слово2 слово".getBytes()));
-        String[] abuse = new String[]{"слово1", "слово2"};
-        new DelAbuse().dropAbuses(System.in, System.out, abuse);
-        assertThat("слово", is(outContent.toString()));
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        delAbuse.dropAbuses(is2, bos, ab);
+        String s = bos.toString();
+        System.out.println(s);
+        boolean contains = false;
+        for (String abuse : ab) {
+            if (s.contains(abuse)) {
+                contains = true;
+                break;
+            }
+        }
+        assertFalse(contains);
     }
 }
