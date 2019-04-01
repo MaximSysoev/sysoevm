006251c3153c37338b39cd9d8f23d6c54793f5b4
Maxim Sysoev <time_amf@mail.ru>
2019-04-01 21:23:31 +0300

    task #106929

--- chapter_004/src/main/java/ru/job4j/InputOuput/Search.java
+++ chapter_004/src/main/java/ru/job4j/InputOuput/Search.java
@@ -4,24 +4,28 @@ import ru.job4j.simpletree.Node;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Search {
 
    private List<String> list = new ArrayList<>();
+
+   public boolean isExts(List<String> exts, String path) {
+       for (int i = 0; i < exts.size(); i++) {
+           if (path.contains(exts.get(i))) {
+                return true;
+           }
+       }
+       return false;
+   }
 
    public void add(File dir, List<String> exts) {
        for (int i = 0; i < dir.listFiles().length; i++) {
            File file = new File(dir.listFiles()[i].getPath());
            String path = file.getName().toString();
            if (!file.isDirectory()) {
-               for (int j = 0; j < exts.size(); j++) {
-                   if (path.contains(exts.get(j))) {
-                       list.add(path);
-                   }
+               if (isExts(exts, path)) {
+                   list.add(path);
                }
            } else {
                add(file, exts);
@@ -38,7 +42,6 @@ public class Search {
        for (int i = 0; i < search.list.size(); i++) {
            System.out.println(search.list.get(i));
        }
-
    }
 
 }
