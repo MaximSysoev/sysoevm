767df5429876e63b00d3406143e3be06280bc091
Maxim Sysoev <time_amf@mail.ru>
2019-04-29 23:21:59 +0300

    task #861

--- chapter_004/src/main/java/ru/job4j/InputOuput/Args.java
+++ chapter_004/src/main/java/ru/job4j/InputOuput/Args.java
@@ -1,8 +1,6 @@
 package ru.job4j.inputouput;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
@@ -10,36 +8,52 @@ import java.util.zip.ZipOutputStream;
 
 public class Args {
 
-    public static void main(String[] args) {
-
-        Path path = Paths.get("/chapter_004/src/main/java/ru/job4j/inputouput/");
-
-        try(
-           ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("C:\\projects\\sysoevm\\project.zip"));
+ /*   public void pack(String path, String exts) {
+       // Path path = Paths.get("/chapter_004/src/main/java/ru/job4j/inputouput/");
+        try (
+                ZipOutputStream out = new ZipOutputStream(new FileOutputStream(path + "\\" + "project.zip"));
         ){
+            File subDir = new File(path);
+            String subdirList[] = subDir.list();
 
-        for (Path element : path) {
-           File file = new File(element.toAbsolutePath().toString());
-           for (int i = 0; i < file.listFiles().length; i++) {
-               File f = new File(file.listFiles()[i].getPath());
-               if (f.isDirectory()) {
-                   String[] files = f.list();
-                   for  (int j = 0; j <files.length; j++) {
-                       zout.putNextEntry(new ZipEntry(files[j]));
-                   }
-               }
-           }
-           break;
-        }
-
-            /*for (int i = 0; i < file.listFiles().length; i++) {
-                FileInputStream fis = new FileInputStream(file.listFiles()[i]);
-                zout.putNextEntry(new ZipEntry(file.listFiles()[i].getName()));
-                int length;
-            }*/
+            for (String sd:subdirList) {
+                String newPath = path + sd;
+                File f = new File(path + sd);
+                if(f.isDirectory())
+                {
+                    String files[] = f.list();
+                    for (int i = 0; i < files.length; i++) {
+                            System.out.println("Adding: " + files[i]);
+                            ZipEntry entry = new ZipEntry(sd +"\\"+files[i]);
+                            out.putNextEntry(entry);
+                    }
+                }
+                else
+                {
+                    ZipEntry entry = new ZipEntry(sd);
+                    out.putNextEntry(entry);
+                }
+            }
+            out.flush();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+*/
+
+    public void pack(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getName()));
+           /* try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.read());
+            }*/
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Args().pack(new File("C:\\projects\\sysoevm\\checkstyle.xml"), new File("C:\\projects\\sysoevm\\project.zip"));
     }
 }
