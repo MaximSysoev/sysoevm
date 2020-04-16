0a41af2a5cf63e155e2237387b1789bf4d446bb8
Maxim Sysoev <time_amf@mail.ru>
2020-04-17 00:42:52 +0300

    task #187589

---
+++ chapter_004/src/main/java/ru/job4j/emailNotification/EmailNotification.java
@@ -0,0 +1,46 @@
+package ru.job4j.emailNotification;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                String subject = String.format("Notification {%s} to email {%s}", user.getUsername(), user.getEmail());
+                String body = String.format("Add a new event to {%s}", user.getUsername());
+                send(subject, "", user.getEmail());
+                System.out.println(String.format("Send to email '%s' successfully!", user.getEmail()));
+            }
+        });
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    public static void main(String[] args) {
+        EmailNotification en = new EmailNotification();
+        en.emailTo(new User("user1", "user1@email"));
+        en.emailTo(new User("user2", "user2@email"));
+        en.emailTo(new User("user3", "user3@email"));
+        en.close();
+
+    }
+
+}
